4fefaef4e35dc6a4c2dccb4c295535bb1e8e41f4
Aashish Garg <aashishgarg@Aashishs-MacBook-Air.local>
2023-05-03 16:21:57 +0530

    enroll

--- airdrop/transfer.ts
+++ airdrop/transfer.ts
@@ -10,32 +10,32 @@ const to = new PublicKey("BPYEeLDEVBvf9ExVYZADUHExHVtHwcz1rjkhvsw6JDaE");
 //Create a Solana devnet connection
 const connection = new Connection("https://api.devnet.solana.com");
 
-// (async () => {
-//     try {
-//         const balance = await connection.getBalance(from.publicKey)
-//         const transaction = new Transaction().add(
-//             SystemProgram.transfer({
-//                 fromPubkey: from.publicKey,
-//                 toPubkey:  to,
-//                 lamports: LAMPORTS_PER_SOL/100,
-//             })
-//         );
-//         transaction.recentBlockhash = (await connection.getLatestBlockhash('confirmed')).blockhash;
-//         transaction.feePayer = from.publicKey;
+(async () => {
+    try {
+        const balance = await connection.getBalance(from.publicKey)
+        const transaction = new Transaction().add(
+            SystemProgram.transfer({
+                fromPubkey: from.publicKey,
+                toPubkey:  to,
+                lamports: LAMPORTS_PER_SOL/100,
+            })
+        );
+        transaction.recentBlockhash = (await connection.getLatestBlockhash('confirmed')).blockhash;
+        transaction.feePayer = from.publicKey;
         
-//         // Sign transaction, broadcast, and confirm
-//         const signature = await sendAndConfirmTransaction(
-//             connection,
-//             transaction,
-//             [from]
-//         );
-//         const balance2 = await connection.getBalance(from.publicKey)
-//         console.log(`Success! Check out your TX here: 
-//         https://explorer.solana.com/tx/${signature}?cluster=devnet`);
-//     } catch(e) {
-//         console.error(`Oops, something went wrong: ${e}`)
-//     }
-// })();
+        // Sign transaction, broadcast, and confirm
+        const signature = await sendAndConfirmTransaction(
+            connection,
+            transaction,
+            [from]
+        );
+        const balance2 = await connection.getBalance(from.publicKey)
+        console.log(`Success! Check out your TX here: 
+        https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    } catch(e) {
+        console.error(`Oops, something went wrong: ${e}`)
+    }
+})();
 
 (async () => {
     try {
